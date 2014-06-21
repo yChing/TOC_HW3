c7e119a9ca1428f4520fa72a762a7fc46418fe29
yChing <ms0777648@gmail.com>
2014-06-22 01:18:40 +0800

    1:18 update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import urllib
-url = 'http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo'
+from sgmllib import SGMLParser
+
+url = 'http://myweb.ncku.edu.tw/~f74002086/a.json'
 fileobj = urllib.urlopen(url)
 print fileobj.read()
